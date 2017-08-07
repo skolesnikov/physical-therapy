c4e4f86dc842ab34d9680f4188fb51a814fd9c45
Ruben Gevorkian <rgevorkian@futrend.com>
2017-08-06 20:56:00 -0400

    Fixes

--- src/environments/global.ts
+++ src/environments/global.ts
@@ -1,3 +1,4 @@
 export const Global = Object.freeze({
      BASE_API_URL: 'http://rubengev.com/pto-api/api' // 'http://localhost/pto-api/api' // 
- });
+     //BASE_API_URL: 'http://10.1.10.43/pto-api/api' 
+    });
